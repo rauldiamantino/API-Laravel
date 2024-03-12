353cfd0590488e1eeae79a4c9589b19b6bfa124c
Raul Diamantino <rauldiamantino25@gmail.com>
2024-03-11 22:46:09 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# API Laravel
+ My First API with Laravel 10
---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\UserController;
+use App\Http\Controllers\Api\V1\InvoiceController;
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/{user}', [UserController::class, 'show']);
+Route::get('/invoices', [InvoiceController::class, 'index']);
+Route::get('/invoices/{invoice}', [InvoiceController::class, 'show']);
